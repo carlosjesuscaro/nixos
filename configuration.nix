5cbb03f5394de2705e562edabb3fb5228e212410
carlos <carlos.jesus.caro@gmail.com>
2025-06-16 22:10:54 -0400

    feat: GRUb setup - partial

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,14 @@
 
   # 2. Bootloader
   # --------------------------------------------------------------------
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    useOSProber = true;
+    efiSupport = true;
+  };
   environment.systemPackages = with pkgs; [
     os-prober
   ];
